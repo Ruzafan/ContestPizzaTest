d7f63c80fd686c4b6772698318bdef9e5ccdb93a
Marc Ramiro <95027357@lidl.de>
2020-02-13 09:07:54 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GoogleContestTest
+{
+    class Program
+    {
+        private static long _maxiumSlices = 0;
+        private static long _diferentTypesOfPizza = 0;
+        private static List<long> _sliceQtt = new List<long>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("App started, add the file path :)");
+            string path = Console.ReadLine();
+            ReadFileAndSetProps(path);
+            Console.WriteLine("File readed.");
+
+
+        }
+
+        private static void ReadFileAndSetProps(string path)
+        {
+            string line;
+            StreamReader file = new StreamReader(path);
+            line = file.ReadLine();
+            SetVarsLine1(line);
+            line = file.ReadLine();
+            SetVarsLine2(line);
+            file.Close();
+        }
+
+        private static void SetVarsLine1(string line)
+        {
+            string MaxiumSlices = string.Empty;
+            string TypesOfPizza = string.Empty;
+            bool isSlices = true;
+
+            foreach (char c in line)
+            {
+                if (c != ' ' && c != '\n')
+                {
+                    if (isSlices)
+                    {
+                        MaxiumSlices += c;
+                    }
+                    else
+                    {
+                        TypesOfPizza += c;
+                    }
+                }
+                else if (c != '\n')
+                {
+                    isSlices = false;
+                }
+            }
+            long.TryParse(MaxiumSlices, out _maxiumSlices);
+            long.TryParse(TypesOfPizza, out _diferentTypesOfPizza);
+
+
+        }
+        private static void SetVarsLine2(string line)
+        {
+            long qtt = 0;
+
+            string sliceqtt = string.Empty;
+            foreach (char c in line)
+            {
+                if (c != ' ' && c != '\n')
+                {
+                    sliceqtt += c;
+                }
+                else if (c != '\n')
+                {
+                    qtt = 0;
+                    long.TryParse(sliceqtt, out qtt);
+                    _sliceQtt.Add(qtt);
+                    sliceqtt = string.Empty;
+                }
+            }
+
+            qtt = 0;
+            long.TryParse(sliceqtt, out qtt);
+            _sliceQtt.Add(qtt);
+            sliceqtt = string.Empty;
+
+        }
+    }
+}
