01fe64d3a3061d05f668c078f4eb35a9d6ef474f
Marc Ramiro <95027357@lidl.de>
2020-02-18 15:59:47 +0100

    first solution

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace GoogleContestTest
 {
@@ -16,8 +17,36 @@ namespace GoogleContestTest
             string path = Console.ReadLine();
             ReadFileAndSetProps(path);
             Console.WriteLine("File readed.");
+            Dictionary<long, int> values = new Dictionary<long, int>();
+            int qtt = 0;
+            foreach (var item in _sliceQtt)
+            {
+                values.Add(qtt, item);
+                qtt++;
+            }
+            values = values.OrderBy(q => q.Value).ToDictionary(x => x.Key, x => x.Value);
+            Dictionary<long, int> result = new Dictionary<long, int>();
+            for (long i = _diferentTypesOfPizza-1; i >= 0; i--)
+            {
+                GetNext(values[i], result,i);
+            }
 
+            Console.WriteLine(string.Join(", ",result.Keys) +'\n' + result.Values.Sum());
+        }
 
+        private static void GetNext(int v, Dictionary<long, int> result,long currPos)
+        {
+            long slices = result.Values.Sum();
+            if (slices < _maxiumSlices)
+            {
+                result.Add(currPos, v);
+            }
+
+            if(result.Values.Sum() > _maxiumSlices)
+            {
+                result.Remove(currPos);
+            }
+            
         }
 
         private static void ReadFileAndSetProps(string path)
@@ -29,12 +58,6 @@ namespace GoogleContestTest
             line = file.ReadLine();
             SetVarsLine2(line);
             file.Close();
-
-
-
-
-
-
         }
 
         private static void SetVarsLine1(string line)
@@ -70,7 +93,7 @@ namespace GoogleContestTest
         {
             int qtt = 0;
             int count = 0;
-            _sliceQtt = new int[_maxiumSlices];
+            _sliceQtt = new int[_diferentTypesOfPizza];
             string sliceqtt = string.Empty;
             foreach (char c in line)
             {
@@ -80,15 +103,12 @@ namespace GoogleContestTest
                 }
                 else if (c != '\n')
                 {
-                    qtt = 0;
                     int.TryParse(sliceqtt, out qtt);
                     _sliceQtt[count] = qtt;
                     sliceqtt = string.Empty;
+                    count++;
                 }
-                count++;
             }
-
-            qtt = 0;
             int.TryParse(sliceqtt, out qtt);
             _sliceQtt[count] = qtt;
             sliceqtt = string.Empty;
